11a3129f89371498b2b47ea415d29b127506537e
profjohnharney <john.harney@centre.edu>
2020-04-19 17:09:12 -0400

    wanquan comment le

--- bots/ali_bot/historybot_test.py
+++ bots/ali_bot/historybot_test.py
@@ -9,21 +9,21 @@
 
 # this is the base code for all history bots as of 10.21.19
 
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
 import os
 import random
 
-GPIO.setmode(GPIO.BCM) #use the GPIO numbering
-GPIO.setwarnings(False) # Avoids warning channel is already in use
-
-led = 21 # GPIO pin 21
-button = 18 # GPIO pin 18
-button_led = 26 # GPIO pin 26
-
-GPIO.setup(led,GPIO.OUT) # sets up pin 21 to output
-GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP) #sets up pin 18 as a button
-GPIO.setup(button_led,GPIO.OUT) #sets up pin 26 to output
+# GPIO.setmode(GPIO.BCM) #use the GPIO numbering
+# GPIO.setwarnings(False) # Avoids warning channel is already in use
+#
+# led = 21 # GPIO pin 21
+# button = 18 # GPIO pin 18
+# button_led = 26 # GPIO pin 26
+#
+# GPIO.setup(led,GPIO.OUT) # sets up pin 21 to output
+# GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP) #sets up pin 18 as a button
+# GPIO.setup(button_led,GPIO.OUT) #sets up pin 26 to output
 
 # idea is to replace following code so that minimal changes are needed per bot
 # will use os.listdir() to get started
@@ -40,28 +40,28 @@ l_dirlist = os.listdir()
 l_audiofiles = []
 
 for file in l_dirlist:
-    if file[-4:] == ".wav":
+    if file[-4:] == ".mp3":
         l_audiofiles.append("'omxplayer -o alsa "  + file + "'")
 
 print(l_audiofiles) # check to see if it worked
 
 
 
-i_count = 0 # set up for correct grammar in notification below
-
-while True:
-        GPIO.output(button_led, True) # turn on button led
-        input_state = GPIO.input(button) # primes the button!
-        if input_state == False:
-            i_count = i_count + 1
-            GPIO.output(button_led, False) # turns off button led
-            GPIO.output(led,True) #Turn on LED
-            #os.system(random.choice(myList)) # play sound file
-            os.system(random.choice(l_audiofiles))
-            GPIO.output(led,False) #turn off LED
-            if i_count == 1:
-                print("History Bot has been activated " + str(i_count) + " time!")
-            else:
-                print("History Bot has been activated " + str(i_count) + " times!")
-            GPIO.output(button_led, True) # turns button led back on
-            time.sleep(0.2)
+# i_count = 0 # set up for correct grammar in notification below
+#
+# while True:
+#         GPIO.output(button_led, True) # turn on button led
+#         input_state = GPIO.input(button) # primes the button!
+#         if input_state == False:
+#             i_count = i_count + 1
+#             GPIO.output(button_led, False) # turns off button led
+#             GPIO.output(led,True) #Turn on LED
+#             #os.system(random.choice(myList)) # play sound file
+#             os.system(random.choice(l_audiofiles))
+#             GPIO.output(led,False) #turn off LED
+#             if i_count == 1:
+#                 print("History Bot has been activated " + str(i_count) + " time!")
+#             else:
+#                 print("History Bot has been activated " + str(i_count) + " times!")
+#             GPIO.output(button_led, True) # turns button led back on
+#             time.sleep(0.2)
