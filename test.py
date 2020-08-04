059592ffc5a56f932fcec9bc9195d9223897460f
profjohnharney <john.harney@centre.edu>
2020-08-04 16:31:36 -0400

    close to final code for 1.1

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-# John Harney, Centre College, updated 08.03.2020
-# THIS IS A TEST
+# John Harney, Centre College, updated 08.04.2020
+# THIS IS FOR HISTORY BOT 1.1, not 1.0
 
 # writing of the button code relied on:
 # Texas-Mark on the official RPi forums: https://www.raspberrypi.org/forums/viewtopic.php?t=176241
@@ -7,7 +7,6 @@
 # Soren at https://raspberrypihq.com/use-a-push-button-with-raspberry-pi-gpio/
 
 import RPi.GPIO as GPIO
-import time # for sleep function below
 import os # for commands via console
 import random
 
@@ -23,7 +22,7 @@ GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP) # sets up pin 18 as a butt
 GPIO.setup(button_led,GPIO.OUT) # sets up pin 26 to output
 
 path = "./" # this is being added so that the script works in Python 2.7
-l_dirlist = os.listdir(path) # Python 2.7 requires an argument; optional in Python 3
+l_dirlist = os.listdir(path) # Python 2.7 requires an argument; argument is optional in Python 3
 l_audiofiles = []
 
 # code takes all mp3 and wav files in the SAME directory as the script
@@ -32,42 +31,27 @@ l_audiofiles = []
 for file in l_dirlist:
     if file[-4:] == ".mp3" or file[-4:] == ".wav":
         l_audiofiles.append("omxplayer -o alsa "  + file)
-
-# this function ensures the same file is not played twice in a row 
-# this does not actually work.... rather than play the same sound twice it will just go back to wait to be activated, having done nothing
-
-# def playFile():
-#     playList = []
-#     playfile = random.choice(l_audiofiles)
-#     while playfile == playList[-1]:
-#         playfile = random.choice(l_audiofiles)
-#     os.system(playfile)
-#     playList.append(playfile)
     
-i_count = 0 # this counter is used so notification uses correct grammar
+i_count = 0 # this counter is used so notifications use correct grammar
 
 # following code is where the magic happens
-# I was able to get the non-repeat to work! Could easily fix so it does not say from the last two... in fact I will... this is all for 1.1
 
 while True: # sets this code on a loop
         GPIO.output(button_led, True) # turn on button led
         input_state = GPIO.input(button) # primes the button!
         playList = l_audiofiles # gets the list ready to avoid repitition of tracks
         if input_state == False: # False == button press
-            i_count = i_count + 1
+            i_count = i_count + 1 
             GPIO.output(button_led, False) # turns off button led
             GPIO.output(led,True) #Turn on LED
-            playfile = random.choice(l_audiofiles)
-            while playfile == playList[-1] or playfile == playList[-2]:
+            playfile = random.choice(l_audiofiles) # selects an audio file from the directory
+            while playfile == playList[-1] or playfile == playList[-2]: # checks to make sure file was not one of last two played
                 playfile = random.choice(l_audiofiles)
-            os.system(playfile)
-            playList.append(playfile)
-            #os.system(random.choice(l_audiofiles)) # takes random file from list and plays through command line
+            os.system(playfile) # plays file via command line
+            playList.append(playfile) # adds played file to playlist to avoid repetition
             GPIO.output(led,False) #turn off LED
             if i_count == 1:
                 print("History Bot has been activated!")
             else:
                 print("History Bot has been activated " + str(i_count) + " times!")
-            print("History Bot spake " + playList[-1]) # test to monitor files played back
             GPIO.output(button_led, True) # turns button led back on
-           # time.sleep(0.2) # brief delay to keep the loop working - turns out I never needed this / will delete in 1.1
